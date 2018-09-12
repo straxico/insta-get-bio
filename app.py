399e0bc858cf92d3e4df6701a59da4e5c0de4b6d
mehran motiee <straxico@gmail.com>
2018-09-13 01:21:49 +0430

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ Created on Wed Sep 12 18:17:16 2018
 """
 import instaloader
 L = instaloader.Instaloader()
-L.login('straxico', '498154724')        # (login)
-profile = instaloader.Profile.from_username(L.context, 'guilan.fanni')
+L.login('#####USERNAME#######', '#####PASSWORD#####')        # (login)
+profile = instaloader.Profile.from_username(L.context, '#####user who get followers bio#####')
 list=[]
 i=0
 for followee in profile.get_followees():
     #user=[]
-    i+=i
+    i+=1
     #user.append(followee.biography)
     #user.append(followee.username)
     #user.append(followee.userid)
@@ -20,5 +20,7 @@ for followee in profile.get_followees():
     #user.append(followee.followers)
     #user.append(followee.followees)
     #list.append(user)
-    list.append(followee.biography)
+    bio=followee.biography
+    list.append(bio)
     print(i)
+    print(bio)
