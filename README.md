c85dc050725aff230cb075eb96609caf4a763a2c
straxico <straxico@gmail.com>
2020-03-07 01:30:27 +0330

    save to csv

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# insta-get-bio
+# instagram get bio
 get bio of all followers of a user by python
+its a simple example of `instaloader.github.io`
+for use
+`git clone https://github.com/straxico/insta-get-bio`
+`cd insta-get-bio`
+
+open app.py with test editor and edit `YOUR_USERNAME` `YOUR_PASSWORD` and `TARGET_USERNAME`
+then run app with `python ./app.py`
+after a few min save all bio in `bio.csv` in the root of project
--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Sep 12 18:17:16 2018
-
 @author: strix
 """
+import csv
 import instaloader
+YOUR_USER_NAME="#########"
+YOUR_PASSWORD="#########"
+TARGET_USERNAME="#########"
+
 L = instaloader.Instaloader()
-L.login('#####USERNAME#######', '#####PASSWORD#####')        # (login)
-profile = instaloader.Profile.from_username(L.context, '#####user who get followers bio#####')
-list=[]
+L.login(YOUR_USER_NAME, YOUR_PASSWORD)        # (login)
+profile = instaloader.Profile.from_username(L.context,TARGET_USERNAME)
+mylist=[]
 i=0
 for followee in profile.get_followees():
     #user=[]
@@ -19,8 +23,12 @@ for followee in profile.get_followees():
     #user.append(followee.full_name)
     #user.append(followee.followers)
     #user.append(followee.followees)
-    #list.append(user)
+    #mylist.append(user)
     bio=followee.biography
-    list.append(bio)
+    mylist.append(bio)
     print(i)
     print(bio)
+
+with open("{}.csv".format(TARGET_USERNAME), 'w',encoding="utf-8") as myfile:
+     wr = csv.writer(myfile)
+     wr.writerow(mylist)
